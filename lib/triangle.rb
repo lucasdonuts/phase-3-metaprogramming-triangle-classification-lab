0257d8825759c2cb7ba2661ecd50fb62b74beb8c
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-09 11:56:22 -0500

    commit tree cbe95f89f61540f4529a927132b3cc7b1b090c28

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+  @@triangle = []
+  
+  def initialize( s1, s2, s3 )
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+    @@triangle = [@s1, @s2, @s3]
+  end
+
+  def kind
+    if self.valid_triangle?
+      triangle = [@s1, @s2, @s3]
+      if triangle.uniq.length == 1
+        return :equilateral
+      elsif triangle.uniq.length == 2
+        return :isosceles
+      elsif triangle.uniq.length == 3
+        return :scalene
+      end
+    else
+      begin
+        raise TriangleError
+        puts TriangleError.message
+      end
+    end
+  end
+
+  def valid_triangle?
+    @@triangle.each do |side|
+      return false if @@triangle.sum - side <= side || side <= 0
+    end
+    return true
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle is invalid"
+    end
+  end
+
 end
